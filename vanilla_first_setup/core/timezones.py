1b2d71fe6c5844dbb838e92e25be2430529a6414
taukakao <joheyy@protonmail.com>
2024-12-08 17:24:15 +0100

    improve timezone backend

--- vanilla_first_setup/core/timezones.py
+++ vanilla_first_setup/core/timezones.py
@@ -2,51 +2,69 @@ import gi
 
 gi.require_version("GWeather", "4.0")
 
-import datetime
+from datetime import datetime
 import logging
+import threading
 
 import requests
 from gi.repository import GLib, GWeather
-from zoneinfo import ZoneInfo
+import pytz
 
 logger = logging.getLogger("FirstSetup::Timezones")
 
-regions: dict[str, dict[str, dict[str, str]]] = {}
 world = GWeather.Location.get_world()
-parents = []
 base = world
-child = None
-while True:
-    child = base.next_child(child)
-    if child is not None:
-        if child.get_level() == GWeather.LocationLevel.REGION:
-            regions[child.get_name()] = {}
-            current_region = child.get_name()
-        elif child.get_level() == GWeather.LocationLevel.COUNTRY:
-            regions[current_region][child.get_name()] = {}
-            current_country = child.get_name()
-        elif child.get_level() == GWeather.LocationLevel.CITY:
-            regions[current_region][current_country][
-                child.get_city_name()
-            ] = child.get_timezone_str()
 
-        if child.next_child(None) is not None:
-            parents.append(child)
-            base = child
-            child = None
-    else:
-        base = base.get_parent()
-        if base is None:
-            break
-        child = parents.pop()
+all_country_codes: list[str] = []
+all_timezones_by_country_code: dict[str, list[str]] = {}
+all_country_names_by_code: dict[str, str] = {}
+user_location = None
+user_country = None
+user_city = None
+user_timezone = None
 
-all_timezones = dict(sorted(regions.items()))
+def register_location_callback(callback):
+    if user_location:
+        callback(user_location)
+    __location_callbacks.append(callback)
+
+def get_timezone_preview(tzname):
+    timezone = pytz.timezone(tzname)
+    now = datetime.now(timezone)
+    now_str = (
+        "%02d:%02d" % (now.hour, now.minute),
+        now.strftime("%A, %d %B %Y"),
+    )
+    return now_str
 
 
-def get_location(callback=None):
+for country_code in pytz.country_timezones:
+    timezones = pytz.country_timezones[country_code]
+    country = world.find_by_country_code(country_code)
+    country_name = country.get_country_name() if country else "Other"
+    all_timezones_by_country_code[country_code] = timezones
+    all_country_names_by_code[country_code] = country_name
+    all_country_codes.append(country_code)
+
+__location_callbacks = []
+
+def __update_user_location(location):
+    global user_location
+    global user_country
+    global user_city
+    global user_timezone
+    user_location = location
+    user_country = location.get_country_name()
+    user_city = location.get_city_name()
+    user_timezone = location.get_timezone().get_identifier()
+
+    for callback in __location_callbacks:
+        GLib.idle_add(callback, location)
+
+def __retrieve_location_thread():
     logger.info("Trying to retrieve timezone automatically")
     try:
-        res = requests.get("http://ip-api.com/json?fields=49344", timeout=3).json()
+        res = requests.get("http://ip-api.com/json?fields=49344", timeout=10).json()
         if res["status"] != "success":
             raise Exception(
                 f"get_location: request failed with message '{res['message']}'"
@@ -56,25 +74,10 @@ def get_location(callback=None):
         logger.error(f"Failed to retrieve timezone: {e}")
         nearest = None
 
+    if nearest:
+        __update_user_location(nearest)
+
     logger.info("Done retrieving timezone")
 
-    if callback:
-        logger.info("Running callback")
-        GLib.idle_add(callback, nearest)
-
-
-tz_preview_cache: dict[str, tuple[str, str]] = {}
-
-
-def get_timezone_preview(tzname):
-    if tzname in tz_preview_cache:
-        return tz_preview_cache[tzname]
-    else:
-        timezone = ZoneInfo(tzname)
-        now = datetime.datetime.now(timezone)
-        now_str = (
-            "%02d:%02d" % (now.hour, now.minute),
-            now.strftime("%A, %d %B %Y"),
-        )
-        tz_preview_cache[tzname] = now_str
-        return now_str
+thread = threading.Thread(target=__retrieve_location_thread)
+thread.start()
