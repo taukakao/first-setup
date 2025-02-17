0703c21a14ef64ab0093d9d208b27bf586d7832e
taukakao <joheyy@protonmail.com>
2025-02-17 23:55:49 +0100

    build translations in test.py

--- test.py
+++ test.py
@@ -22,14 +22,28 @@ VERSION = 'testing'
 
 path_of_this_file = os.path.dirname(os.path.realpath(__file__))
 pkgdatadir = os.path.join(path_of_this_file)
-localedir = os.path.join(path_of_this_file, "build/po")
+localedir = os.path.join(path_of_this_file, "localegen")
 moduledir = os.path.join(path_of_this_file, "vanilla_first_setup")
 
-if __name__ == '__main__':
+def setup_translations():
+    import pathlib
+    po_file_path = pathlib.Path(os.path.join(pkgdatadir, "po"))
 
+    for po_file in po_file_path.glob("*.po"):
+        lang = po_file.stem
+        mo_path = os.path.join(localedir, lang, "LC_MESSAGES")
+        os.makedirs(mo_path, exist_ok=True)
+        mo_file_path = os.path.join(mo_path, "vanilla-first-setup.mo")
+        subprocess.run(["msgfmt", "-o", mo_file_path, po_file.absolute()])
+
+def setup_gresource():
     resource_file = os.path.join(moduledir, "vanilla-first-setup.gresource")
     command = ["glib-compile-resources", f"--sourcedir={moduledir}", f"--target={resource_file}", f"{resource_file}.xml"]
     subprocess.run(command, check=True)
+
+if __name__ == '__main__':
+    setup_translations()
+    setup_gresource()
 
     from vanilla_first_setup import main
     sys.exit(main.main(VERSION, moduledir, localedir))
